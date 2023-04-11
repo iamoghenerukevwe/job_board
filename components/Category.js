7f598b630ab05b4f65e885d5b04fd1c5b6f9f0f1
iamoghenerukevwe <50581464+iamoghenerukevwe@users.noreply.github.com>
2023-04-11 13:28:44 +0100

    continue

--- components/Category.js
+++ components/Category.js
@@ -1,11 +1,26 @@
 import styles from '@/styles/Category.module.css'
+import  Link from 'next/link';
 
 export default function Category() {
 
     return(
 
         <div className={styles.explore}>
+            <div>Explore by category</div>
 
+            <div className={styles.group}>
+
+                <div className={styles.item}>
+                   <Link>
+                        <div className={styles.png}></div>
+                        <div className={styles.title}>UI/UX Design</div>
+                        <div className={styles.small}>UI/UX design is the art of creating user-friendly and visually appealing interfaces that enhance the user's overall experience.</div>
+                   </Link>
+                </div>
+
+            </div>
+
+            
         </div>
 
     )
