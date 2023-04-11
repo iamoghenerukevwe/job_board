b82466d04246b28903fa373b02b177b064c1ca85
iamoghenerukevwe <50581464+iamoghenerukevwe@users.noreply.github.com>
2023-04-11 14:37:58 +0100

    WIP

--- components/Category.js
+++ components/Category.js
@@ -6,15 +6,58 @@ export default function Category() {
     return(
 
         <div className={styles.explore}>
-            <div>Explore by category</div>
+
+            <div className={styles.info}>
+                5 different fields
+                over 3k unique companies
+                listings under 72hours
+            </div>
+
+            <div className={styles.header}>Explore by category</div>
 
             <div className={styles.group}>
 
                 <div className={styles.item}>
-                   <Link>
-                        <div className={styles.png}></div>
+                   <Link href="https://google.com/">
+                        <div className={styles.png_1}></div>
                         <div className={styles.title}>UI/UX Design</div>
-                        <div className={styles.small}>UI/UX design is the art of creating user-friendly and visually appealing interfaces that enhance the user's overall experience.</div>
+                        <div className={styles.small}>Art of creating user-friendly and visually appealing interfaces that enhance the user's overall experience.</div>
+                   </Link>
+                </div>
+
+
+                <div className={styles.item}>
+                   <Link href="https://google.com/">
+                        <div className={styles.png_2}></div>
+                        <div className={styles.title}>Technical Writing</div>
+                        <div className={styles.small}>Creating user-friendly documentation for complex subjects.</div>
+                   </Link>
+                </div>
+
+
+                <div className={styles.item}>
+                   <Link href="https://google.com/">
+                        <div className={styles.png_3}></div>
+                        <div className={styles.title}>Web Development</div>
+                        <div className={styles.small}>Building and maintaining websites and web applications using programming languages and frameworks.</div>
+                   </Link>
+                </div>
+
+
+                <div className={styles.item}>
+                   <Link href="https://google.com/">
+                        <div className={styles.png_4}></div>
+                        <div className={styles.title}>Python</div>
+                        <div className={styles.small}>Versatile programming language used for web development, data analysis, and AI, with a large library of tools.</div>
+                   </Link>
+                </div>
+
+
+                <div className={styles.item}>
+                   <Link href="https://google.com/">
+                        <div className={styles.png_5}></div>
+                        <div className={styles.title}>Cyber Security</div>
+                        <div className={styles.small}>Protecting systems and data from unauthorized access, theft, and damage using various technologies and processes.</div>
                    </Link>
                 </div>
 
